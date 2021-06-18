a42f4496dcc1a344d8078b541e08d3956f9b6004
baixi123 <1426221905@qq.com>
2021-06-18 09:26:47 +0800

    update lib 3

--- pipe_lib3.c
+++ pipe_lib3.c
@@ -1,57 +1,72 @@
-#include <stdio.h>  
-#include <stdlib.h>  
-#include <unistd.h>  
-#include <sys/wait.h>  
-#include <sys/types.h>  
-#define READ    0  
-#define WRITE   1  
-int main(void)  
-{  
-    int x;  
-    pid_t pid;  
-    int pipe1[2],pipe2[2];  
-    /*初始化管道*/  
-    pipe(pipe1);  
-    pipe(pipe2);  
-    pid = fork();  
-    if(pid < 0)  
-    {  
-        printf("create process error!/n");  
-        exit(1);  
-    }  
-    if(pid == 0)        //子进程  
-    {  
-        close(pipe1[WRITE]);  
-        close(pipe2[READ]);  
-        do  
-        {  
-            read(pipe1[READ],&x,sizeof(int));  
-            printf("child %d read: %d\n",getpid(),x++);  
-            write(pipe2[WRITE],&x,sizeof(int));  
-        }while(x<=9);  
-        //读写完成后,关闭管道  
-        close(pipe1[READ]);  
-        close(pipe2[WRITE]);  
-        exit(0);  
-    }  
-    else if(pid > 0) //父进程  
-    {  
-        close(pipe2[WRITE]);  
-        close(pipe1[READ]);  
-        x = 1;  
-        //每次循环向管道1 的1 端写入变量X 的值,并从  
-        //管道2 的0 端读一整数写入X 再对X 加1，直到X 大于10  
-        do{  
-            write(pipe1[WRITE],&x,sizeof(int));  
-            read(pipe2[READ],&x,sizeof(int));  
-            printf("parent %d read: %d\n",getpid(),x);
-	    x=0;  
-        }while(x<=9);  
-        //读写完成后,关闭管道  
-        close(pipe1[WRITE]);  
-        close(pipe2[READ]);  
-        waitpid(pid,NULL,0);  
-        exit(0);  
-    }  
-}  
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
+#define MAXL 100
 
+char buf[MAXL];
+
+int main()
+{
+	pid_t pid;
+	int fd[2];
+	if (pipe(fd) == -1)
+	{
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+	pid = fork();
+	if (pid == 0)
+	{
+		/* Child 1 */
+		if (write(fd[1], "Child process 1 is sending a message!", 38) == -1)
+		{
+			perror("child 1 sending");
+			exit(EXIT_FAILURE);
+		}
+		printf("Child 1 send OK\n");
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		/* Parent */
+		int status;
+		waitpid(pid, &status, WUNTRACED | WCONTINUED);
+		if (read(fd[0], buf, MAXL) == -1)
+		{
+			perror("parent read child 1");
+			exit(EXIT_FAILURE);
+		}
+		printf("Parent read child 1 ok: %s\n", buf);
+	}
+	pid = fork();
+	if (pid == 0)
+	{
+		/* Child 2 */
+		if (write(fd[1], "Child process 2 is sending a message!", 38) == -1)
+		{
+			perror("child 2 sending");
+			exit(EXIT_FAILURE);
+		}
+		printf("Child 2 send OK\n");
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		/* Parent */
+		int status;
+		waitpid(pid, &status, WUNTRACED | WCONTINUED);
+		if (read(fd[0], buf, MAXL) == -1)
+		{
+			perror("parent read child 2");
+			exit(EXIT_FAILURE);
+		}
+		printf("Parent read child 2 ok: %s\n", buf);
+	}
+	close(fd[0]);
+	close(fd[1]);
+	return 0;
+}
